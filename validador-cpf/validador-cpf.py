e41f6cce877c9a7354456373f6d11e39e889abc6
nitroknight1 <contato.passos@protonmail.com>
2022-05-29 02:12:27 -0300

    Adicionando anotações

--- validador-cpf/validador-cpf.py
+++ validador-cpf/validador-cpf.py
@@ -1,17 +1,16 @@
-# xxx.xxx.xxx-xx
+# xxx.xxx.xxx-xx    ---> CPF
+# 1º digito       2º digito
 
-# 1º digito / 2º digito
-
-# x * 10 / # x * 11
-# x * 9 / # x * 10
-# x * 8 / # x * 9
-# x * 7 / # x * 8
-# x * 6 / # x * 7
-# x * 5 / # x * 6
-# x * 4 / # x * 5
-# x * 3 / # x * 4
-# x * 2 / # x * 3
-#         # x * 2 --> primeiro dígito.
+# x * 10           x * 11
+# x * 9            x * 10
+# x * 8            x * 9
+# x * 7            x * 8
+# x * 6            x * 7
+# x * 5            x * 6
+# x * 4            x * 5
+# x * 3            x * 4
+# x * 2            x * 3
+#                  x * 2 --> primeiro dígito.
 
 # Primeiro digito: 11 - (soma dos valores multiplicados % 11) = x 
 # se x > 9 o primeiro digito é igual a 0. o primeiro digito será adicionado na próxima multiplicação.
@@ -20,29 +19,29 @@
 from statistics import multimode
 
 while True:
-    cpf = input('Digite seu CPF completo: ')
-    cpf_backup = cpf # --> Armazenando o valor do CPF inserido antes de tirar as pontuações.
+    cpf = input('Digite seu CPF completo: ') # --> CPF que será inserido
+    cpf_backup = cpf # --> Armazenando o valor do CPF inserido para ser utilizado na verificação
     cpf = cpf.replace('.', '') # --> Removendo as pontuações
-    cpf = cpf.replace('-', '') # --> 
-    cpf = cpf[0:9]
+    cpf = cpf.replace('-', '') # --> Removendo as pontuações
+    cpf = cpf[0:9] # --> Fatiando os nove primeiros dígitos já sem as pontuações
 
-    multiplicador = 10
-    soma = 0
+    multiplicador = 10 # --> Será utilizado para multiplicar os números do CPF
+    soma = 0 # --> Soma total que será aplicada na fórmula: 11 - (soma % 11)
 
-    for n in cpf: # --> multiplicação e soma do primeiro dígito
+    for n in cpf: # --> Multiplicação e soma do primeiro dígito
         n = int(n)
         soma += n * multiplicador
         multiplicador -= 1
     digit_1 = 11 - (soma % 11) # --> Primeiro dígito
 
-    if digit_1 > 9: # --> Transformando o número em "0" caso ele seja maior que 9.
+    if digit_1 > 9: # --> Transformando o número em "0" caso ele seja maior que 9
         digit_1 = 0
-    cpf += str(digit_1) # --> Inserção do primeiro dígito para ser iterado no próximo laço.
+    cpf += str(digit_1) # --> Inserção do primeiro dígito para ser iterado no próximo laço
 
-    multiplicador = 11
+    multiplicador = 11 # --> Defindo para onze para calcular o segundo dígito do CPF
     soma = 0 
     
-    for n in cpf: # --> multiplicação e soma do segundo dígito
+    for n in cpf: # --> Multiplicação e soma do segundo dígito
         n = int(n)
         soma += n * multiplicador
         multiplicador -= 1
@@ -51,7 +50,7 @@ while True:
     # --> Fazendo a junção dos dígitos 
     novo_cpf = cpf_backup[:12] + str(digit_1) + str(digit_2)
 
-    # Abaixo há a verficação do CPF junto com uam varificação de que os números não são sequências.
+    # Abaixo há a verficação do CPF junto com uma varificação de que os números não são sequências
     if novo_cpf == cpf_backup and cpf_backup[0:3] != cpf_backup[4:7] and cpf_backup[0:3] != cpf_backup[8:11]: 
         print('Válido.')
     else:
